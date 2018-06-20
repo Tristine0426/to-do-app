db3d1454449833458b20e89c71f22c219cf0f36f
Tristine Nguyen <tristine@mail.usf.edu>
2018-06-20 14:57:50 -0400

    Assignment 5 completed

--- app.js
+++ app.js
@@ -2,54 +2,53 @@ function onReady() {
     const addToDoForm = document.getElementById('addToDoForm');
     const newToDoText = document.getElementById('newToDoText');
     const toDoList = document.getElementById('toDoList');
-    
+
     addToDoForm.addEventListener('submit', event => {
-        event.preventDefault();
-        
-        // get the text
-        let title = newToDoText.value;
-        
-        // create a new li
-        let newLi = document.createElement('li');
+       event.preventDefault();
 
-        // create a new input
-        let checkbox = document.createElement('input');
+       // get the text
+       let title = newToDoText.value;
 
-        // set the input's type to checkbox
-        checkbox.type = "checkbox";
-        
-        // set the title
-        newLi.textContent = title;
+       // create a new li
+       let newLi = document.createElement('li');
 
-        // attach the checkbox to the li
-        newLi.appendChild(checkbox);
-        
-        // attach the li to the ul
-        toDoList.appendChild(newLi);
-        
-        //empty the input
-        newToDoText.value = '';
-    });
+       // create a new input
+       let checkbox = document.createElement('input');
+
+       // set the input's type to checkbox
+       checkbox.type = "checkbox";
+
+       // set the title
+       newLi.textContent = title;
+
+       // attach the checkbox to the li
+       newLi.appendChild(checkbox);
+
+       // attach the li to the ul
+       toDoList.appendChild(newLi);
+
+       //empty the input
+       newToDoText.value = '';
+
+       // add delete button
+       let deletebtn = document.createElement('BUTTON');
+
+       // create a text node
+       let deleteName = document.createTextNode('delete');
+
+       // append the text to <button>
+       deletebtn.appendChild(deleteName);
+
+       // append <button> to <form>
+       newLi.appendChild(deletebtn);
+
+        deletebtn.addEventListener('click', function() {
+           toDoList.removeChild(newLi);
+       });
+   });
 
 }
 
 window.onload = function() {
-    onReady();
+   onReady();
 };
-
-        // add delete button
-        let deletebtn = document.createElement('BUTTON');
-        
-        // create a text node
-        let t = document.createTextNode('delete');
-        
-        // append the text to <button>
-        deletebtn.appendChild(t);
-        
-        // append <button> to <form>
-        document.body.appendChild(deletebtn);
-
-deletebnt.addEventListener('click', function() {
-            let deleteLi = newToDoText.value;
-            toDoList.removeChild(deleteLi);
-        });
